87bb7258db4b64b2f89ec85eacb6836032633216
Red Artist <redartofcode@gmail.com>
2022-09-09 20:08:04 +0530

    update deps and version bump

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
-use egui_backend::{GfxBackend, UserApp, WindowBackend};
 pub use egui_backend;
 pub use egui_backend::egui;
+use egui_backend::{GfxBackend, UserApp, WindowBackend};
 pub use egui_render_wgpu;
 pub use egui_window_glfw_passthrough;
 /// just impl the `UserApp<egui_window_glfw_passthrough::GlfwWindow, egui_render_wgpu::WgpuBackend>` trait
@@ -9,11 +9,9 @@ pub use egui_window_glfw_passthrough;
 pub fn start_overlay(
     app: impl UserApp<egui_window_glfw_passthrough::GlfwWindow, egui_render_wgpu::WgpuBackend> + 'static,
 ) {
-    let (glfw_backend, window_info_for_gfx) = egui_window_glfw_passthrough::GlfwWindow::new(
-        Default::default(),
-        egui_backend::GfxApiConfig::Vulkan {},
-    );
-    let wgpu_backend = egui_render_wgpu::WgpuBackend::new(window_info_for_gfx, Default::default());
+    let mut glfw_backend =
+        egui_window_glfw_passthrough::GlfwWindow::new(Default::default(), Default::default());
+    let wgpu_backend = egui_render_wgpu::WgpuBackend::new(&mut glfw_backend, Default::default());
 
     glfw_backend.run_event_loop(wgpu_backend, app);
 }
