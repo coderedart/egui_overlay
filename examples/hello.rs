03491fe952721ee449eac68c06245df479ea3728
Red Artist <redartofcode@gmail.com>
2023-06-05 02:23:09 +0530

    version bump for new deps

--- examples/hello.rs
+++ examples/hello.rs
@@ -1,31 +1,73 @@
-use egui_backend::UserApp;
-use egui_overlay::start_overlay;
-use egui_render_wgpu::WgpuBackend;
+use egui_backend::{egui::DragValue, WindowBackend};
+use egui_overlay::EguiOverlay;
+use egui_render_three_d::ThreeDBackend;
+
+fn main() {
+    egui_overlay::start(HelloWorld {
+        text: "hello world".to_string(),
+    });
+}
 
 pub struct HelloWorld {
     pub text: String,
 }
-impl UserApp<egui_window_glfw_passthrough::GlfwWindow, WgpuBackend> for HelloWorld {
-    fn run(
+impl EguiOverlay for HelloWorld {
+    fn gui_run(
         &mut self,
         egui_context: &egui_backend::egui::Context,
-        glfw_backend: &mut egui_window_glfw_passthrough::GlfwWindow,
-        _: &mut WgpuBackend,
+        _three_d_backend: &mut ThreeDBackend,
+        glfw_backend: &mut egui_window_glfw_passthrough::GlfwBackend,
     ) {
         egui_backend::egui::Window::new("hello window").show(egui_context, |ui| {
             ui.text_edit_multiline(&mut self.text);
         });
 
+        // just some controls to show how you can use glfw_backend
+        egui_backend::egui::Window::new("controls").show(egui_context, |ui| {
+            // sometimes, you want to see the borders to understand where the overlay is.
+            if ui.button("toggle borders").clicked() {
+                let dec = glfw_backend.window.is_decorated();
+                glfw_backend.window.set_decorated(!dec);
+            }
+            // how to change size.
+            // WARNING: don't use drag value, because window size changing while dragging ui messes things up.
+            let size = glfw_backend.window.get_size();
+            let mut size = [size.0 as f32, size.1 as f32];
+            let mut changed = false;
+            ui.horizontal(|ui| {
+                ui.label("width: ");
+                ui.add_enabled(false, DragValue::new(&mut size[0]));
+                if ui.button("inc").clicked() {
+                    size[0] += 10.0;
+                    changed = true;
+                }
+                if ui.button("dec").clicked() {
+                    size[0] -= 10.0;
+                    changed = true;
+                }
+            });
+            ui.horizontal(|ui| {
+                ui.label("height: ");
+                ui.add_enabled(false, DragValue::new(&mut size[1]));
+                if ui.button("inc").clicked() {
+                    size[1] += 10.0;
+                    changed = true;
+                }
+                if ui.button("dec").clicked() {
+                    size[1] -= 10.0;
+                    changed = true;
+                }
+            });
+            if changed {
+                glfw_backend.set_window_size(size);
+            }
+        });
+        // here you decide if you want to be passthrough or not.
         if egui_context.wants_pointer_input() || egui_context.wants_keyboard_input() {
             glfw_backend.window.set_mouse_passthrough(false);
         } else {
             glfw_backend.window.set_mouse_passthrough(true);
         }
+        egui_context.request_repaint();
     }
-}
-
-fn main() {
-    start_overlay(HelloWorld {
-        text: "hello world".to_string(),
-    });
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,83 @@
 pub use egui_backend;
 pub use egui_backend::egui;
-use egui_backend::{GfxBackend, UserApp, WindowBackend};
-pub use egui_render_wgpu;
+use egui_backend::{egui::Context, BackendConfig, GfxBackend, UserApp, WindowBackend};
+pub use egui_render_three_d;
+use egui_render_three_d::ThreeDBackend;
 pub use egui_window_glfw_passthrough;
-/// just impl the `UserApp<egui_window_glfw_passthrough::GlfwWindow, egui_render_wgpu::WgpuBackend>` trait
-/// for your App and pass it to this function. this will initialize the glfw window and wgpu backend.
-/// And enters the event loop running the `UserApp::run` fn that you implemented for your app.
-pub fn start_overlay(
-    app: impl UserApp<egui_window_glfw_passthrough::GlfwWindow, egui_render_wgpu::WgpuBackend> + 'static,
-) {
-    let mut glfw_backend =
-        egui_window_glfw_passthrough::GlfwWindow::new(Default::default(), Default::default());
-    let wgpu_backend = egui_render_wgpu::WgpuBackend::new(&mut glfw_backend, Default::default());
+use egui_window_glfw_passthrough::{GlfwBackend, GlfwConfig};
+/// After implementing [`EguiOverlay`], just call this function with your app data
+pub fn start<T: EguiOverlay + 'static>(user_data: T) {
+    let mut glfw_backend = GlfwBackend::new(
+        GlfwConfig {
+            glfw_callback: Box::new(|gtx| {
+                (egui_window_glfw_passthrough::GlfwConfig::default().glfw_callback)(gtx);
+                gtx.window_hint(
+                    egui_window_glfw_passthrough::glfw::WindowHint::ScaleToMonitor(true),
+                );
+            }),
+            ..Default::default()
+        },
+        BackendConfig {
+            is_opengl: true,
+            opengl_config: Default::default(),
+            transparent: Some(true),
+        },
+    );
+    glfw_backend.set_always_on_top(true);
+    glfw_backend.window.set_decorated(false);
+    let three_d_backend = ThreeDBackend::new(&mut glfw_backend, Default::default());
+    let overlap_app = OverlayApp {
+        user_data,
+        egui_context: Default::default(),
+        three_d_backend,
+        glfw_backend,
+    };
+    GlfwBackend::run_event_loop(overlap_app);
+}
+/// Implement this trait for your struct containing data you need. Then, call [`start`] fn with that data
+pub trait EguiOverlay {
+    fn gui_run(
+        &mut self,
+        egui_context: &Context,
+        three_d_backend: &mut ThreeDBackend,
+        glfw_backend: &mut GlfwBackend,
+    );
+}
+pub struct OverlayApp<T: EguiOverlay> {
+    pub user_data: T,
+    pub egui_context: Context,
+    pub three_d_backend: ThreeDBackend,
+    pub glfw_backend: GlfwBackend,
+}
 
-    glfw_backend.run_event_loop(wgpu_backend, app);
+impl<T: EguiOverlay> OverlayApp<T> {}
+
+impl<T: EguiOverlay> UserApp for OverlayApp<T> {
+    type UserGfxBackend = ThreeDBackend;
+
+    type UserWindowBackend = GlfwBackend;
+
+    fn get_all(
+        &mut self,
+    ) -> (
+        &mut Self::UserWindowBackend,
+        &mut Self::UserGfxBackend,
+        &egui::Context,
+    ) {
+        (
+            &mut self.glfw_backend,
+            &mut self.three_d_backend,
+            &self.egui_context,
+        )
+    }
+
+    fn gui_run(&mut self) {
+        let OverlayApp {
+            user_data,
+            egui_context,
+            three_d_backend: wgpu_backend,
+            glfw_backend,
+        } = self;
+        user_data.gui_run(egui_context, wgpu_backend, glfw_backend);
+    }
 }
